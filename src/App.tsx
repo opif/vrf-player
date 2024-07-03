8c7ebf6ee787601bf429dd0bc297f2c5b188ba07
Artur Raczyński <artur.raczynski@gmail.com>
2024-07-04 00:26:48 +0200

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import './App.css'
+import { styled } from 'styled-components';
 
-function App() {
-
+const App = () => {
   return (
     <>
-      <h1>Opowiadania z Ventrilo</h1>      
+      <Wrapper>
+        <Title>Opowiadania z Ventrilo</Title>
+      </Wrapper>
     </>
-  )
-}
+  );
+};
 
-export default App
+const Title = styled.h1`
+  font-size: 3.2em;
+  line-height: 1.1;
+  display: block;
+`;
+
+const Wrapper = styled('div')`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+export default App;
